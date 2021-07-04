6774172359427e4b0d5fd735595f64fa64a20346
liubangyao <a.today.13th.blackdays@gmail.com>
2021-07-04 15:15:43 +0800

    feat(main): 更新版本, 日曆

--- app/src/main/java/com/google/samples/apps/sunflower/utilities/Constants.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/utilities/Constants.kt
@@ -19,5 +19,5 @@ package com.google.samples.apps.sunflower.utilities
 /**
  * Constants used throughout the app.
  */
-const val DATABASE_NAME = "sunflower-db"
+const val DATABASE_NAME = "bw-db"
 const val PLANT_DATA_FILENAME = "plants.json"
